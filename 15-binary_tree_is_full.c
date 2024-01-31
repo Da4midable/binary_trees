c647ead1ff1f51a2bd07bb06cd71529c500d78fc
Saheed Kehinde Yusuf <108232561+Da4midable@users.noreply.github.com>
2024-01-31 20:35:36 +0100

    15++

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -7,13 +7,20 @@
  *
  * Return: 1 if the binary tree is full, 0 otherwise.
  */
+
+
+
 int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left && tree->right && tree->parent)
-		return (1);
+	if (tree)
+	{
+		if (tree->left && tree->right && tree->parent)
+			return (1);
+
+	}
 
 	binary_tree_is_full(tree->left);
 	binary_tree_is_full(tree->right);
