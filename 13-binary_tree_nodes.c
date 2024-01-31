f3863997eb89053711eca84103cabd65126a683b
Saheed Kehinde Yusuf <108232561+Da4midable@users.noreply.github.com>
2024-01-31 18:12:28 +0100

    Betty check

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_nodes - Counts the nodes with at least one child in a binary tree.
+ * binary_tree_nodes - Counts the nodes with at least one child in binary tree.
  *
  * @tree: Pointer to the root node of the tree.
  *
@@ -13,5 +13,6 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	return (tree->left || tree->right ? 1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right) : 0);
+	return (tree->left || tree->right ? 1 + binary_tree_nodes(tree->left)
+			+ binary_tree_nodes(tree->right) : 0);
 }
