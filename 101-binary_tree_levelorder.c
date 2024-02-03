1f4ca4a25b42092136297d9dcf6f91c17f554fc8
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2024-02-03 14:20:32 +0100

    New 101

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -24,13 +24,16 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 	if (!tree->parent)
 		func(tree->n);
 
-	if (tree->left)
-		func(tree->left->n);
+	if (tree)
+	{
+		if (tree->left)
+			func(tree->left->n);
 
-	if (tree->right)
-		func(tree->right->n);
+		if (tree->right)
+			func(tree->right->n);
 
-	binary_tree_levelorder(tree->left, func);
-	binary_tree_levelorder(tree->right, func);
+		binary_tree_levelorder(tree->left, func);
+		binary_tree_levelorder(tree->right, func);
+	}
 
 }
